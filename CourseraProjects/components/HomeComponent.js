d6ada0760a504ab7d353eec14f719ba07d6be410
AfraGngr <gungorafra93@gmail.com>
2020-10-31 00:23:35 +0300

    Modified Home Page

--- CourseraProjects/components/HomeComponent.js
+++ CourseraProjects/components/HomeComponent.js
@@ -1,17 +1,48 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
-    View,
+    ScrollView,
     Text,
+    View
 } from 'react-native';
+import { Card } from "react-native-elements"
+import {DISHES, LEADERS, PROMOTIONS} from "../shared";
 
-const Home = () => {
+function RenderItem(props){
+    const item = props.item;
+
+    if (item != null) {
+        return (
+            <Card
+                featuredTitle = {item.name}
+                featuredSubtitle = {item.designation}
+                image = {require("./images/buffet.png")}
+            >
+                <Text style = {{margin:10}}>{item.description}</Text>
+            </Card>
+        )
+    }
+}
+
+const Home = (props) => {
+    const [dishes, setDishes] = useState(DISHES)
+    const [leaders, setLeaders] = useState(LEADERS)
+    const [promotions, setPromotions] = useState(PROMOTIONS)
+
+    // const a = leaders.filter((dish) => dish.featured)
+    // console.log(a)
+    
     return(
         <View>
-            <Text>
-                Home Component
-            </Text>
+            <ScrollView>
+                <RenderItem item = {dishes.filter((dish) => dish.featured)[0]}/>
+                <RenderItem item = {promotions.filter((promo) => promo.featured)[0]}/>
+                <RenderItem item = {leaders.filter((lead) => lead.featured)[1]}/>
+            </ScrollView>
         </View>
     )
 }
 
 export default Home;
+
+
+
---
+++ CourseraProjects/shared/index.js
@@ -0,0 +1,4 @@
+export * from "./comments"
+export * from "./dishes"
+export * from "./leaders"
+export * from "./promotions"
